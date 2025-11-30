413b10c09558540cb567a3017f2a1679aa549254
Yash Sharma <yashparth50@gmail.com>
2025-12-01 01:50:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
 import MyWork from './Components/MyWork/MyWork'
 import Contact from './Components/Contact/Contact'
-import Footer from './Components/footer/footer'
+import Footer from './Components/Footer/Footer'
 
 const App = () => {
     return (
